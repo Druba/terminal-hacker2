fc1a694c3cd3f95f29b94dfa220145020a228ca7
Druba <andrasmorvay@gmail.com>
2021-01-17 12:03:13 -0800

    NewCommit

---
+++ Terminal Hacker 2/Assets/WM2000/Hacker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terminal.WriteLine("Wifi has been slow so you check the router and see some unwanted neighborhood visitors have been limiting your streaming capacity! To get back to bingeing The Office again, you need to hack in and reroute them to your annoying neighbor Steve.");
+        Terminal.WriteLine("Who do you hack first?");
+        Terminal.WriteLine("1. Hack into local Kindergaden.");
+        Terminal.WriteLine("2. Hack into the plant nursery next door.");
+        Terminal.WriteLine("3. Hack into the cheese factory across the street.");
+        Terminal.WriteLine("Enter your selection: ");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
