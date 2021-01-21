4003f1141d3d5b5e18f5b492a2e465f4edee3a46
Druba <andrasmorvay@gmail.com>
2021-01-20 18:11:53 -0800

    Update Hacker.cs

--- Terminal Hacker 2/Assets/WM2000/Hacker.cs
+++ Terminal Hacker 2/Assets/WM2000/Hacker.cs
@@ -7,19 +7,38 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        ShowMainMenu("Andrew");
+
+    }
+
+    void ShowMainMenu(string name)
+    {
+        /**
         Terminal.WriteLine("Wifi has been slow so you check the router and see some unwanted neighborhood visitors have been limiting your streaming capacity! To get back to bingeing The Office again, you need to hack in and reroute them to your annoying neighbor Steve.");
         Terminal.WriteLine("Who do you hack first?");
         Terminal.WriteLine("1. Hack into local Kindergaden.");
         Terminal.WriteLine("2. Hack into the plant nursery next door.");
         Terminal.WriteLine("3. Hack into the cheese factory across the street.");
         Terminal.WriteLine("Enter your selection: ");
+        **/
 
-
+        Terminal.ClearScreen();
+        /**var greeting = "Hello Andrew";
+        Terminal.WriteLine(greeting);**/
+        Terminal.WriteLine("Hello " + name);
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1 to hack into Kindergarden.");
+        Terminal.WriteLine("Press 2 to hack into plant nursery.");
+        Terminal.WriteLine("Press 3 to hack into cheese factory.");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter your selection: ");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
