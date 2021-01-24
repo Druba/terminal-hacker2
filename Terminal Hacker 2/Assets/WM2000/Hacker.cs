28f6aa0cb320b0692926e38639239f02983d7a54
Druba <andrasmorvay@gmail.com>
2021-01-23 17:16:48 -0800

    Levels of Passwords and Switch

--- Terminal Hacker 2/Assets/WM2000/Hacker.cs
+++ Terminal Hacker 2/Assets/WM2000/Hacker.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    // Game Configuration Data
+
+    string[] Place = { "school", "nursery", "factory" };
+    string[] Level1Passwords = { "ball", "swing", "crayon", "paint", "play" };
+    string[] Level2Passwords = { "succulent", "garden", "bamboo", "orchid", "manure" };
+    string[] Level3Passwords = { "pasteurization", "curdling", "tyrosemiophilia", "mozzarella", "cheesemonger" };
+
     // Game State
     int level;
     enum Screen { MainMenu, Password, Win};
     Screen currentScreen;
-
-
-
+    string password;
 
     // Start is called before the first frame update
     void Start()
@@ -23,19 +28,9 @@ public class Hacker : MonoBehaviour
 
     void ShowMainMenu(/*string name*/)
     {
-        /**
-        Terminal.WriteLine("Wifi has been slow so you check the router and see some unwanted neighborhood visitors have been limiting your streaming capacity! To get back to bingeing The Office again, you need to hack in and reroute them to your annoying neighbor Steve.");
-        Terminal.WriteLine("Who do you hack first?");
-        Terminal.WriteLine("1. Hack into local Kindergaden.");
-        Terminal.WriteLine("2. Hack into the plant nursery next door.");
-        Terminal.WriteLine("3. Hack into the cheese factory across the street.");
-        Terminal.WriteLine("Enter your selection: ");
-        **/
         currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
-        /**var greeting = "Hello Andrew";
-        Terminal.WriteLine(greeting);**/
-       // Terminal.WriteLine("Hello " + name);
+
         Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("");
         Terminal.WriteLine("Press 1 to hack into Kindergarden.");
@@ -55,27 +50,22 @@ public class Hacker : MonoBehaviour
         {
             RunMainMenu(input);
         }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
         
     }
 
     void RunMainMenu(string input)
     {
-        if (input == "1")
+        bool IsValidLevel = (input == "1" || input == "2" || input == "3");
+        if (IsValidLevel)
         {
-            level = 1;
-            StartGame();
+            level = int.Parse(input);
+            StartGame();    
         }
-        else if (input == "2")
-        {
-            level = 2;
-            StartGame();
-        }
-        else if (input == "3")
-        {
-            level = 3;
-            StartGame();
-        }
-        else if (input == "007")
+        else if (input == "007") // easter egg
         {
             Terminal.WriteLine("Please select a level Mr. Bond.");
         }
@@ -93,8 +83,37 @@ public class Hacker : MonoBehaviour
 
     void StartGame()
     {
-        Terminal.WriteLine("You have chosen level " + level);
-        Terminal.WriteLine("Please enter your password: ");
         currentScreen = Screen.Password;
+        switch(level)
+        {
+            case 1:
+                password = Level1Passwords[0];
+                break;
+            case 2:
+                password = Level2Passwords[0];
+                break;
+            case 3:
+                password = Level3Passwords[0];
+                break;
+            default:
+                Debug.LogError("Invalid level number.");
+                break;
+        }
+        Terminal.ClearScreen();
+        Terminal.WriteLine("You have chosen to hack the " + Place[level-1]);
+        Terminal.WriteLine("Please enter the password: ");
+    }
+
+    void CheckPassword(string input)
+    {
+        if (input == password)
+            {
+            currentScreen = Screen.Win;
+            Terminal.WriteLine("Access granted.");
+        }
+        else
+        {
+            Terminal.WriteLine("Incorrect password. Try again.");
+        }
     }
 }
